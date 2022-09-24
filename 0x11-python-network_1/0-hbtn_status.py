88b3c3e1f138002ea42f432c96c46d75d5c89194
alx-system_engineering_devops <89623556+DevepProd@users.noreply.github.com>
2022-09-24 05:25:06 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
-
 #!/usr/bin/python3
-""" Get status in https://intranet.hbtn.io/status"""
-import urllib.request
+"""Fetches https://intranet.hbtn.io/status"""
+
 
 if __name__ == "__main__":
+    import urllib.request
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:")
-        print("\t- type: {}\n\t- content: {}".format(type(html), html))
-        print("\t- utf8 content: {}".format(html.decode('UTF-8')))
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
