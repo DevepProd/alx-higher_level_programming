cf9e0626fa32ed1414a3eb2c988eee007af4727c
alx-system_engineering_devops <89623556+DevepProd@users.noreply.github.com>
2022-09-24 05:27:16 +0200

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""Check status"""
-import requests
+"""Fetches https://intranet.hbtn.io/status"""
 
-
-def status():
-    """status"""
-    result = requests.get("https://intranet.hbtn.io/status")
-
-    print("Body response:")
-    print("\t- type: {}".format(type(result.text)))
-    print("\t- content: {}".format(result.text))
 
 if __name__ == "__main__":
-    status()
+    import requests
+
+    r = requests.get('https://intranet.hbtn.io/status')
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
